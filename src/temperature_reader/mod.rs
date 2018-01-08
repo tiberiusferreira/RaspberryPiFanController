854c78a4214f4016f4904fef9a82680ac24091a1
Tiberio D A R Ferreira <tiberiusferreira@gmail.com>
2018-01-08 15:33:36 -0200

    Wait for processes so they don't become zombies

--- src/temperature_reader/mod.rs
+++ src/temperature_reader/mod.rs
@@ -2,7 +2,7 @@ use std::process::{Command, Stdio};
 pub fn get_temperature() -> f64{
     let read_temperature_command = "/opt/vc/bin/vcgencmd";
     let read_temperature_argument = "measure_temp";
-    let temperature_command_result = Command::new(read_temperature_command)
+    let mut temperature_command_process_handle = Command::new(read_temperature_command)
         .arg(read_temperature_argument)
         .stdout(Stdio::piped())
         .spawn()
@@ -16,10 +16,13 @@ pub fn get_temperature() -> f64{
     let grep_command = "egrep";
     let grep_filter = "[0-9.]{4,}";
     let grep_print_only_matching = "-o";
+    let temperature_command_stdout = temperature_command_process_handle.stdout
+        .take()
+        .expect("Could not take stdout from the temperature_command_process_handle");
     let grep_result = Command::new(grep_command)
         .arg(grep_filter)
         .arg(grep_print_only_matching)
-        .stdin(temperature_command_result.stdout.unwrap())
+        .stdin(temperature_command_stdout)
         .output()
         .unwrap_or_else(|e| {
             panic!("Could no filter the temperature number from the string using:\n{} {} {}:\n{}",
@@ -28,12 +31,13 @@ pub fn get_temperature() -> f64{
                    grep_print_only_matching,
                    e);
         });
-
+    temperature_command_process_handle.wait().expect("Couldn't wait on the temperature command. Maybe the command was not running? Weird.");
     let temp_as_str = String::from_utf8_lossy(&grep_result.stdout).trim().to_string();
     let temperature = temp_as_str.parse::<f64>().unwrap_or_else(|e|{
         panic!("Could not parse the string:\n{}\ninto a f64 value:\n{}",
                temp_as_str,
                e);
     });
+
     temperature
 }
